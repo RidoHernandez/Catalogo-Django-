716cdda3040b4f2af9d697ba5de29fee15552f19
Alan Fonseca <ls23120218@surguanajuato.tecnm.mx>
2026-09-21 17:47:29 -0600

    Agregar configuracion de base de datos a los ajustes

--- catalogo/settings.py
+++ catalogo/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'catalogo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventario',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost', 
+        'PORT': '3306',
     }
 }
 
